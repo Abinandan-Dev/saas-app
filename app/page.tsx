59a143ab5545b64200822f2089180307243aaa89
Abinandan-Dev <devabinandan@gmail.com>
2025-08-16 10:44:50 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='text-2xl underline'>Home Page</h1>
+      <Button>Let's Start Go</Button>
+    </div>
+  )
+}
+
+export default Page
