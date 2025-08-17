f4e75bb8c30d2fbe8cad8f5678dbb5090d167b42
Abinandan-Dev <devabinandan@gmail.com>
2025-08-17 06:58:56 +0530

    Implementation Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,53 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
+import CompanionCard from "../components/companionsCard/CompanionCard";
+import CompanionList from "../components/CompanionList/CompanionList";
+import CTA from "../components/CTA/CTA";
+import { recentSessions } from "../constants";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Home Page</h1>
-      <Button>Let's Start Go</Button>
-    </div>
-  )
-}
+    <main>
+      <h1>Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="1"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="Science"
+          duration={45}
+          color="#E5D0FF"
+        />
 
-export default Page
+        <CompanionCard
+          id="2"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="Maths"
+          duration={30}
+          color="#FFDA6E"
+        />
+
+        <CompanionCard
+          id="3"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="Language"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionList
+          title="Recent Sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
+  );
+};
+
+export default Page;
